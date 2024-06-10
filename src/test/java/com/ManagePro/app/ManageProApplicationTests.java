12faf0a7433922bc67ed8327f763cd98a052f2ca
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-06-10 21:24:11 +0530

    commit

---
+++ src/test/java/com/ManagePro/app/ManageProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManagePro.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
